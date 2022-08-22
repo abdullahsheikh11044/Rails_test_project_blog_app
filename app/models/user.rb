9a2941b62981090be0254d14b885260f9c68c554
abdullahsheikh1104 <abdullah.rafiq@devsinc.com>
2022-08-22 16:30:42 +0500

    Authentication using Devise: 1. Added Devise 2. Added Header in Navigation Bar 3. Devise lockable 4. Devise confirmable 5. Stylled Views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  #ruby after_update :send_autopilot_confirmation, if: :should_send_confirmation
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :lockable, :confirmable
 end
