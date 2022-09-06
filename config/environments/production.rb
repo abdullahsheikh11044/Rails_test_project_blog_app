4bd86e122af9ecee391443d3dc660e2846daa776
abdullahsheikh1104 <abdullah.rafiq@devsinc.com>
2022-09-06 17:37:12 +0500

    updated production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,18 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials[:mail][:username],
+    password: Rails.application.credentials[:mail][:password],
+    domain: 'localhost',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
