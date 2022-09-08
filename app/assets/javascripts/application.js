fb29184476addace9d189a82fc034b660bfa898b
abdullahsheikh1104 <abdullah.rafiq@devsinc.com>
2022-09-08 20:24:24 +0500

    Refactoring of application js and scss error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require trix
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require_tree .
